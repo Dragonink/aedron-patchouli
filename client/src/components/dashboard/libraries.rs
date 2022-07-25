ea9cf10e844a840638c9715500c190a4d325fc76
Dragonink <Dragonink@users.noreply.github.com>
2022-07-25 19:12:57 +0200

    WIP: dialog in libraries

--- client/src/components/dashboard/libraries.rs
+++ client/src/components/dashboard/libraries.rs
@@ -1,11 +1,11 @@
 use aedron_patchouli_common::libraries::LibraryConfig;
 use const_format::formatcp;
-use std::{ops::Deref, str::FromStr};
 use sycamore::{component::Prop, prelude::*};
 use wasm_bindgen::JsCast;
 
 #[component]
 pub(super) fn Libraries<G: Html>(cx: Scope) -> View<G> {
+	use super::{ManageDialog, ManageDialogProps};
 	use aedron_patchouli_common::libraries::{LibraryConfig, API_ENDPOINT};
 	use sycamore::{
 		builder::prelude::*,
@@ -21,16 +21,22 @@ pub(super) fn Libraries<G: Html>(cx: Scope) -> View<G> {
 		managed_library_sig,
 		libraries,
 	};
-	let manage_library_props = ManageLibraryProps {
-		dialog_ref,
-		managed_library_sig,
-		libraries,
-	};
 	let props = SuspenseProps::builder()
 		.children(Children::new(cx, move |cx| {
 			FetchedLibraries(cx, fetched_libraries_props)
 		}))
 		.fallback(t("Loading..."))
+		.build();
+	let dialog_props = ManageDialogProps::builder()
+		.item_desc("library")
+		.dialog_ref(dialog_ref)
+		.items(libraries)
+		.managed_item_sig(managed_library_sig)
+		.children(Children::new(cx, |cx| {
+			LibraryFormFields(cx, managed_library_sig)
+		}))
+		.item_name(|library| &library.name)
+		.req_input(|library| format!("{API_ENDPOINT}/{}", library.id))
 		.build();
 
 	main()
@@ -56,10 +62,10 @@ pub(super) fn Libraries<G: Html>(cx: Scope) -> View<G> {
 					}
 				});
 			})
-			.c(LibraryFormFields(cx, None))
+			.c(LibraryFormFields(cx, create_signal(cx, None)))
 			.c(button().attr("type", "submit").t("Create library")))
 		.c(Suspense(cx, props))
-		.c(ManageLibrary(cx, manage_library_props))
+		.c(ManageDialog(cx, dialog_props))
 		.view(cx)
 }
 
@@ -108,152 +114,20 @@ async fn FetchedLibraries<'a, G: Html>(
 	ul().c(Indexed(cx, props)).view(cx)
 }
 
-#[derive(Prop)]
-struct ManageLibraryProps<'a, G: Html> {
-	dialog_ref: &'a NodeRef<G>,
-	managed_library_sig: &'a ReadSignal<Option<LibraryConfig>>,
-	libraries: &'a Signal<Vec<LibraryConfig>>,
-}
 #[component]
-fn ManageLibrary<'a, G: Html>(cx: Scope<'a>, props: ManageLibraryProps<'a, G>) -> View<G> {
-	use aedron_patchouli_common::libraries::API_ENDPOINT;
-	use std::convert::Infallible;
-	use sycamore::builder::prelude::*;
-	use web_sys::{
-		Event, FormData, Headers, HtmlButtonElement, HtmlDialogElement, HtmlFormElement, Request,
-		RequestInit, UrlSearchParams,
-	};
-
-	#[derive(Debug, Clone, PartialEq, Eq)]
-	enum DialogValue {
-		Cancel,
-		Delete,
-		Update(String),
-	}
-	impl DialogValue {
-		pub const CANCEL: &'static str = "cancel";
-		pub const DELETE: &'static str = "delete";
-	}
-	impl FromStr for DialogValue {
-		type Err = Infallible;
-
-		fn from_str(s: &str) -> Result<Self, Self::Err> {
-			Ok(if s == Self::CANCEL {
-				Self::Cancel
-			} else if s == Self::DELETE {
-				Self::Delete
-			} else {
-				Self::Update(s.to_string())
-			})
-		}
-	}
-
-	let ManageLibraryProps {
-		dialog_ref,
-		managed_library_sig,
-		libraries,
-	} = props;
-
-	dialog()
-		.bind_ref(dialog_ref.clone())
-		.on("close", move |_| {
-			let library = managed_library_sig.get();
-			let library = if let Some(library) = library.deref() {
-				library
-			} else {
-				return;
-			};
-
-			let mut req = RequestInit::new();
-			match dialog_ref
-				.get::<DomNode>()
-				.unchecked_into::<HtmlDialogElement>()
-				.return_value()
-				.parse()
-				.unwrap()
-			{
-				DialogValue::Cancel => {
-					return;
-				}
-				DialogValue::Delete => {
-					if !web_sys::window()
-						.unwrap()
-						.confirm_with_message(&format!(
-							"Are you sure you want to delete the library {:?}?\nWorry not, your media files will not be deleted.",
-							library.name
-						))
-						.unwrap()
-					{
-						return;
-					}
-					req.method("DELETE");
-				}
-				DialogValue::Update(data) => {
-					let headers = Headers::new().unwrap();
-					headers
-						.append("content-type", "application/x-www-form-urlencoded")
-						.unwrap();
-					req.method("PUT").headers(&headers).body(Some(&data.into()));
-				}
-			}
-			let req =
-				Request::new_with_str_and_init(&format!("{API_ENDPOINT}/{}", library.id), &req)
-					.unwrap();
-			sycamore::futures::spawn_local_scoped(cx, async move {
-				match crate::send_api(&req).await.unwrap() {
-					204 => libraries.modify().retain(|user| {
-						user.ne(managed_library_sig.get().as_ref().as_ref().unwrap())
-					}),
-					_ => web_sys::window()
-						.unwrap()
-						.alert_with_message(
-							"Something went wrong on the server.\nPlease try again.",
-						)
-						.unwrap(),
-				}
-			});
-		})
-		.c(form()
-			.attr("method", "dialog")
-			.dyn_c(move || LibraryFormFields(cx, managed_library_sig.get().as_ref().clone()))
-			.c(button()
-				.attr("type", "submit")
-				.bool_attr("formnovalidate", true)
-				.attr("value", DialogValue::CANCEL)
-				.t("Cancel"))
-			.c(button()
-				.attr("type", "submit")
-				.bool_attr("formnovalidate", true)
-				.attr("value", DialogValue::DELETE)
-				.t("Delete library"))
-			.c(button()
-				.attr("type", "submit")
-				.on("click", |ev: Event| {
-					let btn = ev.target().unwrap().unchecked_into::<HtmlButtonElement>();
-					let form = btn
-						.parent_element()
-						.unwrap()
-						.unchecked_into::<HtmlFormElement>();
-					let form_data = FormData::new_with_form(&form).unwrap();
-					let search_params = UrlSearchParams::new_with_str_sequence_sequence(&form_data)
-						.unwrap()
-						.to_string();
-					btn.set_value(&String::from(&search_params));
-				})
-				.t("Update library")))
-		.view(cx)
-}
-
-#[component]
-fn LibraryFormFields<G: Html>(cx: Scope, library: Option<LibraryConfig>) -> View<G> {
+fn LibraryFormFields<'a, G: Html>(
+	cx: Scope<'a>,
+	library: &'a ReadSignal<Option<LibraryConfig>>,
+) -> View<G> {
 	use aedron_patchouli_common::libraries::LibraryKind;
 	use sycamore::builder::prelude::*;
 	use web_sys::{Event, HtmlInputElement};
 
-	let library = create_ref(cx, library);
 	let paths_sig = create_signal(
 		cx,
 		library
+			.get()
+			.as_ref()
 			.as_ref()
 			.map(|library| (0..=library.paths.len()).collect())
 			.unwrap_or_else(|| vec![0]),
@@ -261,9 +135,12 @@ fn LibraryFormFields<G: Html>(cx: Scope, library: Option<LibraryConfig>) -> View
 	let indexed_props = IndexedProps::builder()
 		.iterable(paths_sig)
 		.view(move |cx, index| {
-			let value = create_signal(
+			View::empty()
+			/* let value = create_signal(
 				cx,
 				library
+					.get()
+					.as_ref()
 					.as_ref()
 					.map(|library| library.paths.get(index).cloned().unwrap_or_default())
 					.unwrap_or_default(),
@@ -298,7 +175,7 @@ fn LibraryFormFields<G: Html>(cx: Scope, library: Option<LibraryConfig>) -> View
 						}
 					}
 				})
-				.view(cx)
+				.view(cx) */
 		})
 		.build();
 
@@ -308,20 +185,20 @@ fn LibraryFormFields<G: Html>(cx: Scope, library: Option<LibraryConfig>) -> View
 			.c(input()
 				.attr("name", "name")
 				.bool_attr("required", true)
-				.attr(
-					"value",
+				.dyn_attr("value", || {
 					library
+						.get()
+						.as_ref()
 						.as_ref()
 						.map(|library| library.name.clone())
-						.unwrap_or_default(),
-				))
+				}))
 			.view(cx),
 		label()
 			.t("Type")
 			.c(select()
 				.attr("name", "kind")
 				.bool_attr("required", true)
-				.bool_attr("disabled", library.is_some())
+				.dyn_bool_attr("disabled", || library.get().is_some())
 				.c(option().attr("value", "").t("— Please select a type —"))
 				.c(View::new_fragment(
 					enum_iterator::all::<LibraryKind>()
@@ -330,13 +207,14 @@ fn LibraryFormFields<G: Html>(cx: Scope, library: Option<LibraryConfig>) -> View
 
 							option()
 								.attr("value", var_s)
-								.bool_attr(
-									"selected",
+								.dyn_bool_attr("selected", move || {
 									library
+										.get()
+										.as_ref()
 										.as_ref()
 										.map(|library| library.kind == var)
-										.unwrap_or_default(),
-								)
+										.unwrap_or_default()
+								})
 								.t(var_s)
 								.view(cx)
 						})
